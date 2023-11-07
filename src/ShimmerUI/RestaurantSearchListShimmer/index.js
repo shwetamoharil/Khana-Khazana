f44588cee5880f195fa69727d0a203317f99bbd2
ShwetaMoharil-GSLab <shweta.moharil@gslab.com>
2023-11-07 13:16:28 +0530

    feat: added shimmer for res search list

---
+++ src/ShimmerUI/RestaurantSearchListShimmer/index.js
@@ -0,0 +1,1 @@
+export { default as RestaurantSearchListShimmer } from "./RestaurantSearchListShimmer";
--- src/ShimmerUI/index.js
+++ src/ShimmerUI/index.js
@@ -2,3 +2,4 @@ export * from "./BodyShimmer";
 export * from "./SearchShimmer";
 export * from "./RestaurantMenuShimmer";
 export * from "./SearchListShimmer";
+export * from "./RestaurantSearchListShimmer";
